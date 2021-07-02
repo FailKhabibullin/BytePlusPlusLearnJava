ce8be2f7071a8b70a26493806d8ab6ee3a00afff
Fail Khabibullin <rendelkey@yandex.ru>
2021-07-02 11:39:30 +0300

    Learn lesson 2 Done

---
+++ src/org/failkhabibullin/lessons/lesson2/Arithmetics.java
@@ -0,0 +1,18 @@
+package org.failkhabibullin.lessons.lesson2;
+
+import java.util.Scanner;
+
+public class Arithmetics {
+
+	private static String in = new Scanner(System.in).nextLine();
+
+	public static void main(String[] args) {
+		int x = 10;
+		double d = 10.;
+
+		String str = "Hello World!";
+
+		double pow = Math.pow(3, 2);
+		System.out.println(Math.floor(pow));
+	}
+}
