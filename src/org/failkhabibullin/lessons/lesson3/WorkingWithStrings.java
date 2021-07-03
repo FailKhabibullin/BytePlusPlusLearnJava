ffd024b77a603ad6c81ee43d00008549ad2496a3
Fail Khabibullin <rendelkey@yandex.ru>
2021-07-03 11:25:18 +0300

    Learn lesson 3 Str manipulation done

---
+++ src/org/failkhabibullin/lessons/lesson3/WorkingWithStrings.java
@@ -0,0 +1,13 @@
+package org.failkhabibullin.lessons.lesson3;
+
+import java.util.Locale;
+
+class WorkingWithStrings {
+	public static void main(String[] args) {
+		String s = "Hello, World!";
+
+		char[] strChars = s.toCharArray();
+
+		System.out.println(strChars);
+	}
+}
